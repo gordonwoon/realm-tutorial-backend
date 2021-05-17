c6afc731ed11504e4058b9297cea4d0d217964f8
Gordon Woon <gordonwoon@live.com>
2021-05-18 00:38:10 +0800

    complete tutorial

--- functions/removeTeamMember/source.js
+++ functions/removeTeamMember/source.js
@@ -6,19 +6,19 @@ exports = async function(email) {
     return {error: `User ${email} not found`};
   }
   const callingUser = context.user;
-  
+
   if (memberToRemove._id === callingUser.id) {
     return {error: "You cannot remove yourself from your team"};
   }
-  
+
   const {canWritePartitions} = memberToRemove;
-  
+
   const projectPartition = `project=${callingUser.id}`;
 
   if ((canWritePartitions == null) || !canWritePartitions.includes(projectPartition)) {
     return {error: `User ${email} is not a member of your team`};
   }
-  
+
   try {
     return await collection.updateOne(
       {_id: memberToRemove._id},
